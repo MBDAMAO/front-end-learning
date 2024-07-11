069358153c5c3e04aedf9425e295820ccd8951e1
Damao <3034906016@qq.com>
2024-07-11 12:59:01 +0800

    hash router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,9 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+  RouteRecordRaw,
+} from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -68,7 +73,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 router.beforeEach((to, from, next) => {
