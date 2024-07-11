f4dcf719c54d57351f32cefc37ba4f5cf88dfe05
damao <3034906016@qq.com>
2024-07-11 11:32:05 +0800

    del webpack

--- vue.config.js
+++ vue.config.js
@@ -7,27 +7,3 @@ module.exports = defineConfig({
     },
   },
 });
-// const path = require("path");
-// module.exports = {
-//   configureWebpack: {
-//     resolve: {
-//       alias: {
-//         "@": path.resolve(__dirname, "src"),
-//       },
-//     },
-//   },
-// };
-// const path = require("path");
-// function resolve(dir) {
-//   return path.join(__dirname, dir);
-// }
-// module.exports = {
-//   chainWebpack: (config) => {
-//     config.resolve.alias
-//       .set("@", resolve("src"))
-//       .set("assets", resolve("src/assets"))
-//       .set("components", resolve("src/components"))
-//       .set("base", resolve("baseConfig"))
-//       .set("public", resolve("public"));
-//   },
-// };
