aa7bfb8bafd04854e75ef17ce842f8ce85542a91
MBDAMAO <92683225+MBDAMAO@users.noreply.github.com>
2024-07-14 04:04:21 +0000

    Deploying to gh-pages from @ MBDAMAO/front-end-learning@78357d26cdb55766c1c188937aa161460d33b501 🚀

--- static/index.vue_vue_type_style_index_0_scoped_dc992d26_lang-CyjeeDaE.js
+++
@@ -1,1 +0,0 @@
-import{l as e}from"./index-C6e5QISR.js";function r(){return e({url:"/video/feed",method:"get"})}function d(){return e({url:"/video/home_feed",method:"get"})}function u(t,o){return e({url:"/video/like/"+t+"/"+o,method:"put"})}function n(){return e({url:"/video/similar",method:"get"})}export{r as a,n as b,d as g,u as l};
